46cb17a2d5a0958ac0da6414674e5fd78a4195fd
jeff.li <lijippy@163.com>
2024-04-18 21:19:48 +0800

    fix cmake

--- CMakeModules/boost_package.cmake
+++ CMakeModules/boost_package.cmake
@@ -8,39 +8,6 @@
 set(Boost_MIN_VER 107000)
 set(Boost_MIN_VER_STR "1.70")
 
-if(NOT
-  ((Boost_VERSION VERSION_GREATER Boost_MIN_VER OR
-    Boost_VERSION VERSION_EQUAL Boost_MIN_VER) OR
-   (Boost_VERSION_STRING VERSION_GREATER Boost_MIN_VER_STR OR
-    Boost_VERSION_STRING VERSION_EQUAL Boost_MIN_VER_STR) OR
-   (Boost_VERSION_MACRO VERSION_GREATER Boost_MIN_VER OR
-    Boost_VERSION_MACRO VERSION_EQUAL Boost_MIN_VER))
-  AND NOT FLY_WITH_EXTERNAL_PACKAGES_ONLY)
-  set(VER 1.70.0)
-  message(WARNING
-      "WARN: Found Boost v${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}."
-      "Minimum required ${VER}. Build will download Boost Compute.")
-  fly_dep_check_and_populate(${boost_prefix}
-    URL_AND_HASH
-    URI https://github.com/boostorg/compute/archive/boost-${VER}.tar.gz
-    REF MD5=e160ec0ff825fc2850ea4614323b1fb5
-  )
-  if(NOT TARGET Boost::boost)
-    add_library(Boost::boost IMPORTED INTERFACE GLOBAL)
-  endif()
-  set_target_properties(Boost::boost PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${${boost_prefix}_SOURCE_DIR}/include;${Boost_INCLUDE_DIR}"
-    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${${boost_prefix}_SOURCE_DIR}/include;${Boost_INCLUDE_DIR}"
-    )
-else()
-  if(NOT TARGET Boost::boost)
-    add_library(Boost::boost IMPORTED INTERFACE GLOBAL)
-    set_target_properties(Boost::boost PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIR}"
-      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIR}")
-  endif()
-endif()
-
 if(TARGET Boost::boost)
   set(BOOST_DEFINITIONS "BOOST_CHRONO_HEADER_ONLY;BOOST_COMPUTE_THREAD_SAFE;BOOST_COMPUTE_HAVE_THREAD_LOCAL")
 
