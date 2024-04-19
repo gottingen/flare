2ddb8a63e25854f4ec8c1d6e7aef8d73584e6b7a
jeff.li <lijippy@163.com>
2024-04-19 15:01:29 +0800

    fix comment

--- src/backend/oneapi/susan.cpp
+++ src/backend/oneapi/susan.cpp
@@ -1,11 +1,17 @@
-/*******************************************************
- * Copyright (c) 2022, Arrayfire
- * all rights reserved.
- *
- * This file is distributed under 3-clause bsd license.
- * the complete license agreement can be obtained at:
- * http://Arrayfire.com/licenses/bsd-3-clause
- ********************************************************/
+// Copyright 2023 The EA Authors.
+// part of Elastic AI Search
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 #include <Array.hpp>
 #include <err_oneapi.hpp>
